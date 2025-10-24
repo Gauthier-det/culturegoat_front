8e840bbe53036a32d4290e6a8e0852702bd77e28
Gauthier-det <gauthier.detroussel@gmail.com>
2025-10-24 14:11:56 +0200

    add link

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", { autoConnect: true });
+const socket = io("https://culturegoat-back.onrender.com", { autoConnect: true });
 
 export default socket;
