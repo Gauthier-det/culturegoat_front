c73253d69f7b2ec180dba8bb2a5dc2fca7e5ba4c
Gauthyeh <124699873+Gauthier-det@users.noreply.github.com>
2025-11-14 12:45:14 +0100

    Change socket connection URL to production server

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001", { autoConnect: true });
+const socket = io("https://culturegoat-back.onrender.com/", { autoConnect: true });
 
 export default socket;
