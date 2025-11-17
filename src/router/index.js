f0552a71b111f0b08071045e9238cc66de31593b
Gauthier-det <gauthier.detroussel@gmail.com>
2025-11-17 19:03:09 +0100

    "add WIP view and route"

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Accueil from "../views/Home.vue";
+import Home from "../views/Home.vue";
 import Room from "../views/Room.vue";
 import Game from "../views/Game.vue";
 import AddQuestion from "../views/AddQuestion.vue";
 import AdminQuestion from "../views/AdminQuestion.vue";
+import WIP from "../views/WIP.vue";
 
 const routes = [
-    { path: "/", component: Accueil },
+    { path: "/", component: WIP },
     { path: "/room/:roomId", component: Room },
     { path: "/game/:roomId", component: Game },
     { path: "/add-question", component: AddQuestion },
